0e20fb1cfe547ef1112df7ca87e05a3f44cf2e72
Gleb Sabirzyanov <gleb.sabirzyanov@gmail.com>
2022-04-14 18:56:05 +1000

    Add Sexypad VIA support

---
+++ keyboards/gleb_sexy/sexypad/config.h
@@ -0,0 +1,55 @@
+/* Copyright 2020 David Philip Barr <@davidphilipbarr>
+ * Copyright 2021 @filterpaper
+ * SPDX-License-Identifier: GPL-2.0+
+ */
+
+#pragma once
+#include "config_common.h"
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID       0x0000
+#define PRODUCT_ID      0x0001
+#define DEVICE_VER      0x0001
+#define MANUFACTURER    Gleb
+#define PRODUCT         Sexypad
+
+/* key matrix size */
+#define MATRIX_ROWS 2
+#define MATRIX_COLS 9
+
+/* key matrix pins */
+#define DIRECT_PINS {   \
+    { B3, B2, B6, NO_PIN, NO_PIN, NO_PIN, NO_PIN, NO_PIN, NO_PIN }, \
+    { B5, B4 }  \
+}
+
+#define ENCODERS_PAD_A { F4, F4, B1 }
+#define ENCODERS_PAD_B { F6, F5, F7 }
+
+#define ENCODER_RESOLUTION 4
+
+#define ENCODERS 3
+
+#define ENCODERS_CW_KEY \
+    {                   \
+        { 3, 0 },       \
+        { 5, 0 },       \
+        { 7, 0 }        \
+    }
+#define ENCODERS_CCW_KEY \
+    {                    \
+        { 4, 0 },        \
+        { 6, 0 },        \
+        { 8, 0 }         \
+    }
+
+#define UNUSED_PINS
+
+#define BOOTMAGIC_LITE_ROW 0
+#define BOOTMAGIC_LITE_COLUMN 0
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 8
+
+/* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
+#define DEBOUNCE 5
+
---
+++ keyboards/gleb_sexy/sexypad_oled/keymaps/default/keymap.c
@@ -0,0 +1,47 @@
+/* Copyright 2020 David Philip Barr <@davidphilipbarr>
+ * Copyright 2021 @filterpaper
+ * SPDX-License-Identifier: GPL-2.0+
+ */
+
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [0] = LAYOUT(
+        KC_A, KC_B, KC_C,
+        KC_D, KC_E, KC_F
+    ),
+    [1] = LAYOUT(
+        _______, _______, _______,
+        _______, QK_BOOT, _______
+    )
+};
+
+#ifdef OLED_ENABLE
+bool oled_task_user(void) {
+    // Host Keyboard Layer Status
+    oled_write_P(PSTR("Hello World!"), false);
+
+    // switch (get_highest_layer(layer_state)) {
+    //     case _QWERTY:
+    //         oled_write_P(PSTR("Default\n"), false);
+    //         break;
+    //     case _FN:
+    //         oled_write_P(PSTR("FN\n"), false);
+    //         break;
+    //     case _ADJ:
+    //         oled_write_P(PSTR("ADJ\n"), false);
+    //         break;
+    //     default:
+    //         // Or use the write_ln shortcut over adding '\n' to the end of your string
+    //         oled_write_ln_P(PSTR("Undefined"), false);
+    // }
+
+    // // Host Keyboard LED Status
+    // led_t led_state = host_keyboard_led_state();
+    // oled_write_P(led_state.num_lock ? PSTR("NUM ") : PSTR("    "), false);
+    // oled_write_P(led_state.caps_lock ? PSTR("CAP ") : PSTR("    "), false);
+    // oled_write_P(led_state.scroll_lock ? PSTR("SCR ") : PSTR("    "), false);
+    
+    return false;
+}
+#endif
