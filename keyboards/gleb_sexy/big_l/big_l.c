10b2749567e7ff802f0d01adc9fe3035fd04bd07
Gleb Sabirzyanov <gleb.sabirzyanov@gmail.com>
2022-04-23 19:21:46 +1000

    Add Big L

---
+++ keyboards/gleb_sexy/big_l/big_l.c
@@ -0,0 +1,63 @@
+/* Copyright 2020 David Philip Barr <@davidphilipbarr>
+ * Copyright 2021 @filterpaper
+ * SPDX-License-Identifier: GPL-2.0+
+ */
+
+#include "big_l.h"
+
+#if !defined(VIA_ENABLE) && defined(ENCODER_ENABLE)
+bool encoder_update_kb(uint8_t index, bool clockwise) {
+    // if (!encoder_update_user(index, clockwise)) { return false; }
+    if (index == 0) {
+        if (clockwise) {
+            tap_code(KC_1);
+        } else {
+            tap_code(KC_2);
+        }
+    } else if (index == 1) {
+        if (clockwise) {
+            tap_code(KC_3);
+        } else {
+            tap_code(KC_4);
+        }
+    } else if (index == 2) {
+        if (clockwise) {
+            tap_code(KC_5);
+        } else {
+            tap_code(KC_6);
+        }
+    } else if (index == 3) {
+        if (clockwise) {
+            tap_code(KC_5);
+        } else {
+            tap_code(KC_6);
+        }
+    } else if (index == 4) {
+        if (clockwise) {
+            tap_code(KC_5);
+        } else {
+            tap_code(KC_6);
+        }
+    } else if (index == 5) {
+        if (clockwise) {
+            tap_code(KC_5);
+        } else {
+            tap_code(KC_6);
+        }
+    } else if (index == 6) {
+        if (clockwise) {
+            tap_code(KC_5);
+        } else {
+            tap_code(KC_6);
+        }
+    } else if (index == 7) {
+        if (clockwise) {
+            tap_code(KC_5);
+        } else {
+            tap_code(KC_6);
+        }
+    }
+    return true;
+}
+#endif
+
---
+++ keyboards/gleb_sexy/big_l/big_l.h
@@ -0,0 +1,16 @@
+/* Copyright 2020 David Philip Barr <@davidphilipbarr>
+ * Copyright 2021 @filterpaper
+ * SPDX-License-Identifier: GPL-2.0+
+ */
+
+#include "quantum.h"
+
+#define LAYOUT( \
+    K00, K01, K02, \
+    K10, K11  \
+) \
+{ \
+    { K00, K01, K02, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO }, \
+    { K10, K11 }  \
+}
+
