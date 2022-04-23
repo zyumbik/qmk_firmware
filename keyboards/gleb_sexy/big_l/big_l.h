2544858d2094656ebcb82a8967e4d260edc4339c
Gleb Sabirzyanov <gleb.sabirzyanov@gmail.com>
2022-04-23 23:36:31 +1000

    Working encoders on Big L

--- keyboards/gleb_sexy/big_l/big_l.h
+++ keyboards/gleb_sexy/big_l/big_l.h
@@ -6,11 +6,13 @@
 #include "quantum.h"
 
 #define LAYOUT( \
-    K00, K01, K02, \
-    K10, K11  \
+    K00, K01, K02, K03, K04, K05, K06, K07, \
+    K00_A, K01_A, K02_A, K03_A, K04_A, K05_A, K06_A, K07_A, \
+    K00_B, K01_B, K02_B, K03_B, K04_B, K05_B, K06_B, K07_B  \
 ) \
 { \
-    { K00, K01, K02, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO }, \
-    { K10, K11 }  \
+    { K00, K01, K02, K03, K04, K05, K06, K07 }, \
+    { K00_A, K01_A, K02_A, K03_A, K04_A, K05_A, K06_A, K07_A }, \
+    { K00_B, K01_B, K02_B, K03_B, K04_B, K05_B, K06_B, K07_B }  \
 }
 
