b6b42aeb1d56c24bd16525e7819d161140146fac
Gleb Sabirzyanov <gleb.sabirzyanov@gmail.com>
2022-04-27 22:54:06 +1000

    Working LED numbers!!!

--- keyboards/gleb_sexy/sexypad/keymaps/default/keymap.c
+++ keyboards/gleb_sexy/sexypad/keymaps/default/keymap.c
@@ -6,8 +6,10 @@
 #include QMK_KEYBOARD_H
 
 #define SHR_LATCH E6
-#define SHR_CLOCK C7
-#define SHR_DATA  D6
+#define SHR_CLOCK C6
+#define SHR_DATA  D7
+ 
+static const int led_num[] = {126,12,182,158,204,218,250,14,254,222};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
@@ -20,18 +22,45 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
 void keyboard_post_init_user(void) {
-    // rgblight_sethsv_at(HSV_WHITE, 0);
-    
-    writePinLow(C6);
+    setPinOutput(SHR_DATA);
+    setPinOutput(SHR_LATCH);
+    setPinOutput(SHR_CLOCK);
 }
+
+static inline void shift_pulse(void) {
+    writePinHigh(SHR_CLOCK);
+    writePinLow(SHR_CLOCK);
+}
+
+static void shift_out(uint8_t value) {
+    writePinLow(SHR_LATCH);
+    for (uint8_t i = 0; i < 8; i++) {
+        if (value & 0b10000000) {
+            writePinHigh(SHR_DATA);
+        } else {
+            writePinLow(SHR_DATA);
+        }
+
+        shift_pulse();
+        value = value << 1;
+    }
+    writePinHigh(SHR_LATCH);
+}
+
+int currentNum = 0;
+static int length = sizeof(led_num) / sizeof(int);
+
 /**/
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    writePinLow(C6);
     if (record->event.key.col==0 && record->event.key.row==0 && record->event.pressed) {
-        writePinHigh(C6);
+        shift_out((uint8_t)led_num[currentNum]);
+        currentNum = (currentNum + 1) % length;
     }
     return true;
 }
+
+
+
 
 /*
 layer_state_t layer_state_set_user(layer_state_t state) {
