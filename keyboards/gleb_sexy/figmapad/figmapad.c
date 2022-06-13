66ecbbcfe5c09be788cd19507bdf9274d4d7d851
Gleb Sabirzyanov <gleb.sabirzyanov@gmail.com>
2022-06-13 12:05:22 +1000

    Remove encoder update function

--- keyboards/gleb_sexy/figmapad/figmapad.c
+++ keyboards/gleb_sexy/figmapad/figmapad.c
@@ -3,24 +3,3 @@
  */
 
 #include "figmapad.h"
-
-#if !defined(ENCODERS) && defined(ENCODER_ENABLE)
-bool encoder_update_kb(uint8_t index, bool clockwise) {
-    // if (!encoder_update_user(index, clockwise)) { return false; }
-    if (index == 0) {
-        if (clockwise) {
-            tap_code(KC_1);
-        } else {
-            tap_code(KC_2);
-        }
-    } else if (index == 1) {
-        if (clockwise) {
-            tap_code(KC_3);
-        } else {
-            tap_code(KC_4);
-        }
-    }
-    return true;
-}
-#endif
-
