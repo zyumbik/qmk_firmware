4bcf007a103e41cdfe33f51dcc645fee4e9448f9
Gleb Sabirzyanov <gleb.sabirzyanov@gmail.com>
2022-12-04 21:24:12 +0600

    Update QMK/minor changes

---
+++ keyboards/glebsexy/skull/skull.c
@@ -0,0 +1,14 @@
+/*const led_config_t g_led_config = { {
+  // Key Matrix to LED Index
+  {   5, NO_LED, NO_LED,   0 },
+  { NO_LED, NO_LED, NO_LED, NO_LED },
+  {   4, NO_LED, NO_LED,   1 },
+  {   3, NO_LED, NO_LED,   2 }
+}, {
+  // LED Index to Physical Position
+  { 188,  16 }, { 187,  48 }, { 149,  64 }, { 112,  64 }, {  37,  48 }, {  38,  16 }
+}, {
+  // LED Index to Flag
+  1, 4, 4, 4, 4, 1
+} };
+*/
