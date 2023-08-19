e9f859f20a92c51a0a04623f4946746ebe4699d9
Gleb Sabirzyanov <gleb.sabirzyanov@gmail.com>
2023-08-19 10:01:06 +0600

    Working keymap

--- keyboards/glebsexy/skull/keymaps/gleb/keymap.c
+++ keyboards/glebsexy/skull/keymaps/gleb/keymap.c
@@ -52,7 +52,7 @@ enum {
 };
 
 //Tap Dance Definitions
-qk_tap_dance_action_t tap_dance_actions[] = {
+tap_dance_action_t tap_dance_actions[] = {
   //Tap once for Esc, twice for Caps Lock
   [TD_ESC_CAPS]  = ACTION_TAP_DANCE_DOUBLE(KC_ESC, KC_CAPS)
 // Other declarations would go here, separated by commas, if you have them
