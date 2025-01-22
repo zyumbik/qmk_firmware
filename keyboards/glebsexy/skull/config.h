6abf7c98c5ad4fed581bea35e6cd4e00caa76920
Gleb Sabirzyanov <gleb@MacBook-Pro-M1-Pro.local>
2025-01-22 23:50:18 +0500

    Increase combo buffer length

--- keyboards/glebsexy/skull/config.h
+++ keyboards/glebsexy/skull/config.h
@@ -6,3 +6,5 @@
 #define SERIAL_USART_FULL_DUPLEX   // Enable full duplex operation mode.
 #define SERIAL_USART_TX_PIN GP0    // USART TX pin
 #define SERIAL_USART_RX_PIN GP1    // USART RX pin
+
+#define COMBO_BUFFER_LENGTH 8
