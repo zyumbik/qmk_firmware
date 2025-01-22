f25f30f540a1df8d485e694897f28130ba492e20
Gleb Sabirzyanov <gleb@MacBook-Pro-M1-Pro.local>
2025-01-22 22:48:11 +0500

    Keymap modifications, add basic gaming keymap

--- keyboards/glebsexy/skull/keymaps/gleb/keymap.c
+++ keyboards/glebsexy/skull/keymaps/gleb/keymap.c
@@ -8,39 +8,52 @@ enum layer_names {
     BASE,
     SYM,
     NAV,
+    GAME,
     EMPTY
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = LAYOUT_split_3x5_2(
-        LCMD_T(KC_Q),   KC_W,           KC_E,   KC_R,   KC_T,            KC_Y,   KC_U,   KC_I,      KC_O,             KC_P,
-        LCMD_T(KC_A),   KC_S,           KC_D,   KC_F,   KC_G,            KC_H,   KC_J,   KC_K,      KC_L,             RALT_T(KC_SCLN),
-        LSFT_T(KC_Z),   LCTL_T(KC_X),   KC_C,   KC_V,   KC_B,            KC_N,   KC_M,   KC_COMM,   RCTL_T(KC_DOT),   RSFT_T(KC_SLSH),
+        LALT_T(KC_Q),   KC_W,           KC_E,           KC_R,   KC_T,            KC_Y,   KC_U,   KC_I,      KC_O,             RCMD_T(KC_P),
+        LCMD_T(KC_A),   KC_S,           KC_D,           KC_F,   KC_G,            KC_H,   KC_J,   KC_K,      KC_L,             RALT_T(KC_SCLN),
+        LSFT_T(KC_Z),   LCTL_T(KC_X),   LALT_T(KC_C),   KC_V,   KC_B,            KC_N,   KC_M,   KC_COMM,   RCTL_T(KC_DOT),   RSFT_T(KC_SLSH),
         
-        KC_LCMD,   LT(SYM, KC_SPC),    // left
-        TT(NAV),   RCMD_T(KC_NO)   // right
+        KC_LCMD,   LT(SYM, KC_SPC),   // left
+        TT(NAV),   KC_RCMD            // right 
         ),
 
+    // LEFT THUMB
     [SYM] = LAYOUT_split_3x5_2(
-        KC_LALT, LALT(KC_LBRC), KC_LCBR, KC_RCBR, KC_GRV,                KC_DEL,  KC_7, KC_8, KC_9, KC_0,
-        KC_LCMD, LALT(KC_BSLS), KC_LPRN, KC_RPRN, LALT(KC_SCLN),         KC_RCTL, KC_4, KC_5, KC_6, KC_RALT,
-        KC_LSFT, KC_LALT, KC_LBRC, KC_RBRC, LSFT(LALT(KC_MINS)),         KC_RCMD, KC_1, KC_2, KC_3, KC_RSFT,
+        KC_LALT,    LALT(KC_LBRC),    KC_LCBR,    KC_RCBR,    KC_GRV,                     KC_DEL,     KC_7,    KC_8,    KC_9,    KC_0,
+        KC_LCMD,    LALT(KC_BSLS),    KC_LPRN,    KC_RPRN,    LALT(KC_SCLN),              KC_RCTL,    KC_4,    KC_5,    KC_6,    KC_RALT,
+        KC_LSFT,    KC_LALT,          KC_LBRC,    KC_RBRC,    LSFT(LALT(KC_MINS)),        KC_RCMD,    KC_1,    KC_2,    KC_3,    KC_RSFT,
 
-        _______, _______, // left
-        TO(NAV), _______  // right
+        // (reset layer, see below)
+        RCMD_T(KC_NO),   _______,        // left
+        TO(NAV),         RCMD_T(KC_NO)   // right
         ),
 
+    // RIGHT THUMB
     [NAV] = LAYOUT_split_3x5_2(
         KC_LALT, KC_MPRV, KC_MPLY, KC_MNXT, KC_WH_D,                     KC_PGUP,  KC_F14,  KC_UP,   KC_F15,   MO(EMPTY),
         KC_LCMD, KC_VOLD, KC_VOLU, KC_MUTE, KC_WH_U,                     KC_PGDN,  KC_LEFT, KC_DOWN, KC_RIGHT, KC_RALT,
-        KC_LSFT, RGB_TOG, RGB_HUI, KC_F3, KC_F4,                         KC_RCMD,  KC_VOLD, KC_VOLU, KC_RCTL,  KC_RSFT,
+        KC_LSFT, KC_F1,   KC_F2,   KC_F3,   KC_F4,                       KC_RCMD,  KC_VOLD, KC_VOLU, KC_RCTL,  KC_RSFT,
 
-        _______, _______, // left
-        TO(NAV), _______  // right
+        // (reset layer, see below)
+        RCMD_T(KC_NO),  _______,        // left
+        TO(NAV),        RCMD_T(KC_NO)   // right
+        ),
+
+    [GAME] = LAYOUT_split_3x5_2(
+        KC_TAB,    KC_Q,       KC_W,       KC_E,       KC_R,         _______,  _______,  KC_UP,   _______,   _______,
+        KC_LCTL,  KC_A,       KC_S,       KC_D,       KC_F,         _______,  KC_LEFT, KC_DOWN, KC_RIGHT,   _______,
+        KC_LSFT, KC_Z,       KC_X,       KC_C,       KC_V,         _______,  KC_J,    KC_I,    KC_K,       KC_L,
+        KC_LALT,   KC_SPC,     // left
+        KC_SPC,    KC_RSFT     // right
         ),
 
     [EMPTY] = LAYOUT_split_3x5_2(
-        QK_BOOT,    _______,    _______,    _______,    _______,         _______,    _______,    _______,    _______,    _______,
+        QK_BOOT,    RGB_TOG,    RGB_HUI,    _______,    _______,         _______,    _______,    _______,    _______,    _______,
         _______,    _______,    _______,    _______,    _______,         _______,    _______,    _______,    _______,    _______,
         _______,    _______,    _______,    _______,    _______,         _______,    _______,    _______,    _______,    _______,
         _______,    _______,    // left
